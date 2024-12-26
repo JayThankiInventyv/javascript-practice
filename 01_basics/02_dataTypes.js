176f1699e3a9cd93bb3e45019812b39415ff707c
Jay Thanki <108997046+jaythanki18@users.noreply.github.com>
2024-12-26 16:32:57 +0000

    data types in js

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,17 @@
+let name = "Jay";
+let age = 20;
+let isLoggedIn = true;
+let state;
+
+// number -> 2 to power 53
+// bigint
+// string -> ""
+// boolean -> true/false
+// null -> standalone value
+// undefined -> variable declared but value not assigned is undefined
+// symbol -> unique identifier
+
+//object
+
+console.log(typeof undefined)
+console.log(typeof null)
